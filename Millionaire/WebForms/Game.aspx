ce0878a24c60240109d91505de86389529b51dc5
Das-Tier <vitalijmogola@gmail.com>
2015-06-19 12:51:23 +0300

    First

---
+++ Millionaire/WebForms/Game.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="false" CodeBehind="Game.aspx.cs" Inherits="WebForms.Game" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Who wants to be a millionaire?</title>
+</head>
+<body>
+    <h1>Хто хоче стати міліонером?</h1>
+    
+    <form id="frm_play" runat="server" method="get" >
+    <div>
+        <asp:Button ID="btn_newGame" Text="Нова гра" runat="server" OnClick="btn_newGame_Click" /><asp:Button ID="btn_exit" Text="Вихід" runat="server" />
+        <asp:Label ID="lbl_question" Text="Запитання" runat="server"></asp:Label>
+        <asp:Panel ID="pnl_param" runat="server">
+            <asp:RadioButtonList ID="rblst_anwer" runat="server" >
+                <asp:ListItem Value="a"></asp:ListItem>
+                <asp:ListItem Value="b"></asp:ListItem>
+                <asp:ListItem Value="c"></asp:ListItem>
+                <asp:ListItem Value="d"></asp:ListItem>
+            </asp:RadioButtonList>
+        </asp:Panel>
+        <asp:Button ID="btn_check" Text="Відповісти" OnClick="btn_check_Click" runat="server"/>
+        <asp:Label ID="lbl_score" Text="Результат:" runat="server"></asp:Label>   
+    </div>
+    </form>
+</body>
+</html>
---
+++ Millionaire/WebForms/Game.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace WebForms
+{
+    public static class QuizTable
+    {
+        public static int index = 0;
+        public static int Score = 0;
+        public static int [] scoreArray = { 0, 100, 200, 300, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 125000, 250000, 500000, 1000000 };
+    }
+    public partial class Game : System.Web.UI.Page
+    {
+        
+        List<Question> questions;
+        public Game()
+        {
+            CreateQuestions();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btn_check_Click(object sender, EventArgs e)
+        {
+            //CreateQuestions();
+            if (rblst_anwer.SelectedValue==questions[QuizTable.index].Answer)
+            {
+                QuizTable.Score = QuizTable.scoreArray[QuizTable.index++];
+                lbl_score.Text = QuizTable.Score.ToString();
+                lbl_question.Text = questions[QuizTable.index++].Text;
+            }
+        }
+
+        protected void btn_newGame_Click(object sender, EventArgs e)
+        {
+            QuizTable.index = 0;
+            QuizTable.Score = 0;
+            lbl_question.Text = questions[QuizTable.index].Text;
+        }
+        private void CreateQuestions()
+        {
+            questions = new List<Question>();
+            QuizTable.Score = 0;
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(Server.MapPath("/App_Data/Data.xml"));
+            XmlElement xRoot = xDoc.DocumentElement;
+            foreach (XmlElement xnode in xRoot)
+            {
+                Question quiz = new Question();
+                XmlNode attr = xnode.Attributes.GetNamedItem("id");
+                if (attr != null)
+                    quiz.ID = Int32.Parse(attr.Value);
+
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "text")
+                        quiz.Text = childnode.InnerText;
+
+                    if (childnode.Name == "a")
+                        quiz.A = childnode.InnerText;
+
+                    if (childnode.Name == "b")
+                        quiz.B = childnode.InnerText;
+
+                    if (childnode.Name == "c")
+                        quiz.C = childnode.InnerText;
+
+                    if (childnode.Name == "d")
+                        quiz.D = childnode.InnerText;
+
+                    if (childnode.Name == "answer")
+                        quiz.Answer = childnode.InnerText;
+                }
+                questions.Add(quiz);
+            }
+        }
+    }
+}
